1119aa346eca600659a4f2d831bfa0f0021d246b
Dmitrii Grechko <prime.optimys@mail.ru>
2025-04-03 21:01:33 +0500

    Add modules: config.py, utils.py. Add file database.ini Module utils.py: add func create_database, add_employers_to_database, add_vacancies_to_database

---
+++ main.py
@@ -0,0 +1,20 @@
+from config import config
+from src.utils import create_database, add_employers_to_database, add_vacancies_to_database
+from src.external_api_hh import HeadHunterAPI
+
+
+def main():
+    params = config()
+    api_hh = HeadHunterAPI()
+    data_emp = api_hh.get_data_employers()
+    data_vac = api_hh.get_data_vacancies()
+    create_database("search_vacancies", params)
+    add_employers_to_database(data_emp, "search_vacancies", params)
+    add_vacancies_to_database(data_vac, "search_vacancies", params)
+
+
+
+
+
+if __name__ == "__main__":
+    main()

85091d82aec55f6f993d54976c61e3a592df59c0
Dmitrii Grechko <prime.optimys@mail.ru>
2025-04-04 10:23:00 +0500

    edit main.py

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from src.db_manager import DBManager
 
 
 def main():
+    """
+    Главная функция запуска программы поиска вакансий
+    """
+    # Запрос пользовательского ввода
     database_name = input("Введите имя базы данных для создания: ")
     search_query = input("Введите поисковую фразу для поиска вакансий по имени: ")
+    # Запрос данных с API HH.ru
     params = config()
     api_hh = HeadHunterAPI()
     data_emp = api_hh.get_data_employers()
     data_vac = api_hh.get_data_vacancies()
+    # Создание базы данных и таблиц
     create_database(database_name, params)
+    # Ввод данных в даблицы БД
     add_employers_to_database(data_emp, database_name, params)
     add_vacancies_to_database(data_vac, database_name, params)
+    # Вывод данных поисковых запросов
     output = DBManager(database_name, params)
     print("\nСписок компаний\n")
     output.get_companies_and_vacancies_count()
